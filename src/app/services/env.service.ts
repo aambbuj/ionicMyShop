bff8415f1692446c422edcd29e8f4bd513648fc6
myshop29 <shop.jagadish@gmail.com>
2020-12-25 11:41:03 +0530

    25.12.2020

--- src/app/services/env.service.ts
+++ src/app/services/env.service.ts
@@ -4,13 +4,13 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EnvService {
-  // API_URL = 'http://localhost/laravelMyShop/public/api/';
-  // IMG_URL = 'http://localhost/laravelMyShop/public/';
-  // paymetURL = 'http://localhost/laravelMyShop/public/payment';
+  API_URL = 'http://localhost/laravelMyShop/public/api/';
+  IMG_URL = 'http://localhost/laravelMyShop/public/';
+  paymetURL = 'http://localhost/laravelMyShop/public/payment';
 
-    API_URL = 'https://my-shop.in/laravelMyShop/public/api/';
-    IMG_URL = 'https://my-shop.in/laravelMyShop/public/';
-    paymetURL = 'http://localhost/laravelMyShop/public/payment';
+    // API_URL = 'https://my-shop.in/laravelMyShop/public/api/';
+    // IMG_URL = 'https://my-shop.in/laravelMyShop/public/';
+    // paymetURL = 'http://localhost/laravelMyShop/public/payment';
 
   constructor() { }
 }
